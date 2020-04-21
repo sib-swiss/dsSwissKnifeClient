6e29cd8761cb1cdf05789f741ab837f5b256e382
Iulian <iulian_dragan@yahoo.com>
2020-04-21 14:40:25 +0200

    dssGetOption, dssSynthpop

--- R/dssSynthpop.R
+++ R/dssSynthpop.R
@@ -1,11 +1,11 @@
 #' @title Generate synthetic data using the synthpop package
 #' @param func a character, the name of the function to call (all functions are available).
-#' @param ... further arguments to be passed to the function (see the documentation of the GRridge package).
+#' @param ... further arguments to be passed to the function (see the documentation of the Synthpop package).
 #' Object names should be passed as character strings.
 #' @param async a logical, see datashield.aggregate
 #' @param wait a logical, see datashield.aggregate
 #' @param  datasources a list of opal objects obtained after logging into the opal servers (see datashield.login)
-#'
+  #'
 #' @export
 #'
 
@@ -21,3 +21,4 @@ dssSynthpop <- function(func, ..., async = TRUE, wait = TRUE, datasources = NULL
   opal::datashield.aggregate(datasources, as.symbol(cally), async = async, wait = wait)
 
 }
+
