2bcc78e5287f6da7f169aec940588d7870328a6b
Iulian <iulian_dragan@yahoo.com>
2020-07-06 15:15:47 +0200

    DSOpal in test setup

--- tests/testthat/setup.R
+++ tests/testthat/setup.R
@@ -1,25 +1,20 @@
-pkgload::load_all('/home/iulian/datashield/dsSwissKnife')
-opals <- dssCreatePseudoServers(servers = 2)
-#logindata <- read.delim('/home/iulian/datashield/logindata_test.txt')
-#opals <- datashield.login(opals, logindata[2,,drop = FALSE])
-#opals <- datashield.login(x)
-
 datashield.aggregate(opals['local1'], as.symbol('partialData("iris", 1, 40)'))
-datashield.aggregate(opals['local2'], as.symbol('partialData("iris", 41, 150)'))
-datashield.aggregate(opals["local1"], as.symbol('fullData("dataFarkas", "GRridge")') )
-data("iris")
-part_iris_1 <- opals$local1$envir$iris
-part_iris_2 <- opals$local2$envir$iris
+pkgload::load_all('/home/iulian/datashield/DSOpal')
 
 
-#datashield.aggregate(opals['local1'], as.symbol('partialData("iris", 1, 40)'))
-#datashield.aggregate(opals['local2'], as.symbol('partialData("iris", 41, 150)'))
-#datashield.aggregate(opals["local1"], as.symbol('fullData("dataFarkas", "GRridge")') )
+options(verbose=FALSE)
 
-#x <- dssCreatePseudoServers(1)
-#logindata <- read.delim('/home/iulian/datashield/logindata_test.txt')
-#logindata  <- logindata[logindata$server == 'local2', ,drop = FALSE]
-#logindata$server <- 'remote2'
-#y <- datashield.login(logindata)
-#opals <- c(y,x)
-#datashield.aggregate(opals, as.symbol('partialData("iris", 1, 40)'), async = FALSE)
+options(opal.username='administrator',
+        opal.password='password',
+
+        opal.url='http://192.168.2.104:8080'
+)
+
+builder <- newDSLoginBuilder()
+builder$append(server="sim1", url=getOption("opal.url"), table="datashield.CNSIM",
+               user=getOption("opal.username"), password=getOption("opal.password"))
+
+logindata <- builder$build()
+
+
+opals <- DSOpal::datashield.login(logins = logindata, assign = TRUE)
