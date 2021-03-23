451b0c9c6e79ec557e19886b1ad5b44d557a1a91
IulianD <iulian_dragan@yahoo.com>
2021-03-23 10:02:03 +0000

    dssScale combined

--- R/dssScale.R
+++ R/dssScale.R
@@ -4,19 +4,70 @@
 #' @param what a character name of the source dataframe
 #' @param center either a logical value or a numeric vector of length equal to the number of columns of "what"
 #' @param scale either a logical value or a numeric vector of length equal to the number of columns of "what"
+#' @param type a character, 'split' or 'combine'. If either center or scaled are set to  TRUE it defines the
+#' scope for the means used for centering and/or standard deviations used for scaling. If 'combine', global values,
+#' calculated accross all nodes are used, in case of 'split' the node local values are used. Default 'combine'.
+
 #' @param async a logical, see datashield.aggregate
 
 #' @param  datasources a list of opal objects obtained after logging into the opal servers (see datashield.login)
 #' @export
 #'
 
-dssScale <- function(symbol, what, center = TRUE, scale = TRUE, async = TRUE, datasources = NULL){
+dssScale <- function(symbol, what, center = TRUE, scale = TRUE, type = 'combine', async = TRUE, datasources = NULL){
+  if(!(type %in% c('combine', 'split'))){
+    stop('Function argument "type" has to be either "combine" or "split"')
+  }
   if(is.null(datasources)){
     datasources <- datashield.connections_find()
   }
-  center <- .encode.arg(center)
-  scale <- .encode.arg(scale)
-  expr <- paste0('scaleDSS(', what, ', "', center, '", "', scale, '")')
-  datashield.assign(datasources, symbol, as.symbol(expr), async = async)
 
+  if(type == 'combine'){
+      useRootMeanSquare <- FALSE
+      if (is.logical(center)){
+      if(center){
+        covs <- dssCov(what, async = async, datasources = datasources)
+        center <- covs$global$means
+      } else {
+        useRootMeanSquare <-TRUE
+      }
+    }
+    ctr <- .encode.arg(center)
+    expr <- paste0('scaleDSS(', what, ', "', ctr, '", FALSE)')
+    datashield.assign(datasources, symbol, as.symbol(expr), async = async)
+
+
+    if(is.logical(scale)){
+      if(scale){
+        if(!useRootMeanSquare){
+          covs <- dssCov(symbol, async = async, datasources = datasources) # of the centered dataframe
+          scale <- sqrt(diag(covs$global$vcov))
+        } else {
+          # root-mean-square bother - execute partSSD with xpoint = 0
+          # I don't know how many numerical columns so:
+          cm <- dssColMeans(symbol, type = 'combine', async = async, datasources = datasources)$global$means
+
+          scale <- unlist(sapply(names(cm), function(x){
+
+            cally <- paste0('partSsd(', symbol,'$',x, ',"', .encode.arg(0), '")')
+            res <- datashield.aggregate(datasources, as.symbol(cally), async = async)
+            lens <- dssSwapKeys(res)[['len']]
+            glob_root_mean_sq <- sum(unlist(dssSwapKeys(res)[['ssd']]))
+            tot_len <- sum(unlist(lens))
+            sqrt(glob_root_mean_sq/(tot_len -1))
+          } ) )
+
+        }
+      }
+     }
+    scale <- .encode.arg(scale)
+    expr <- paste0('scaleDSS(',symbol, ', FALSE ', ', "', scale, '")') # it's already centered
+    datashield.assign(datasources, symbol, as.symbol(expr), async = async)
+
+  } else {
+    center <- .encode.arg(center)
+    scale <- .encode.arg(scale)
+    expr <- paste0('scaleDSS(',what, ',"',center, '","', scale, '")')
+    datashield.assign(datasources, symbol, as.symbol(expr), async = async)
+  }
 }
--- tests/testthat/test-dssScale.R
+++ tests/testthat/test-dssScale.R
@@ -1,5 +1,32 @@
-test_that("dssScale works", {
-  dssScale('iris_scaled', 'iris', datasources = opals["server1"])
-  x <- as.data.frame(scale(session1$iris[,1:4]))
-  expect_equal(summary(session1$iris_scaled[,2:5]), summary(x))
+test_that("dssScale_TRUE_TRUE works", {
+  dssScale('iris_scaled', 'iris', center = TRUE, scale = TRUE, datasources = opals)
+  x <- as.data.frame(scale(iris[,1:4],center = TRUE, scale=TRUE))
+  expect_equal(round(rbind(session1$iris_scaled[,2:5],session2$iris_scaled[,2:5] ),2), round(x,2))
+  xdef <- as.data.frame(scale(iris[,1:4]))
+  dssScale('iris_scaled_def', 'iris',  datasources = opals)
+  expect_equal(round(rbind(session1$iris_scaled_def[,2:5],session2$iris_scaled_def[,2:5] ),2), round(xdef,2))
 })
+test_that("dssScale_FALSE_FALSE works", {
+  dssScale('iris_scaled', 'iris', center = FALSE, scale = FALSE, datasources = opals)
+  x <- as.data.frame(scale(iris[,1:4],center = FALSE, scale=FALSE))
+  expect_equal(round(rbind(session1$iris_scaled[,2:5],session2$iris_scaled[,2:5] ),2), round(x,2))
+})
+
+test_that("dssScale_TRUE_FALSE works", {
+  dssScale('iris_scaled', 'iris', center = TRUE, scale = FALSE, datasources = opals)
+  x <- as.data.frame(scale(iris[,1:4],center = TRUE, scale=FALSE))
+  expect_equal(round(rbind(session1$iris_scaled[,2:5],session2$iris_scaled[,2:5] ),2), round(x,2))
+})
+
+test_that("dssScale_FALSE_TRUE works", {
+  dssScale('iris_scaled', 'iris', center = FALSE, scale = TRUE, datasources = opals)
+  x <- as.data.frame(scale.default(iris[,1:4],center = FALSE, scale=TRUE))
+  expect_equal(round(rbind(session1$iris_scaled[,2:5],session2$iris_scaled[,2:5] ),2), round(x,2))
+})
+
+test_that("dssScale_SOMETHING_SOMETHING works", {
+  dssScale('iris_scaled', 'iris', center = 1:4, scale = 1:4, datasources = opals)
+  x <- as.data.frame(scale.default(iris[,1:4],center = 1:4, scale=1:4))
+  expect_equal(round(rbind(session1$iris_scaled[,2:5],session2$iris_scaled[,2:5] ),2), round(x,2))
+})
+
