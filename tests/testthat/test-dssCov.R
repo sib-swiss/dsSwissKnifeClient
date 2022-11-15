8617c1f78bf2ecd4d52a29a2e26615f931051cae
IulianD <iulian_dragan@yahoo.com>
2022-11-15 11:23:02 +0100

    test-dssCov for data frame column (and .betterExtract)

--- tests/testthat/test-dssCov.R
+++ tests/testthat/test-dssCov.R
@@ -17,3 +17,12 @@ test_that("dssCov weighted works", {
   rem <- dssCov('iris', type = 'split', wt = 'w',  datasources = opals[1])
   expect_true(all(rem$server1$cov - cov_iris1$cov == 0))
 })
+
+test_that("dssCov weighted works with weights in a data frame", {
+  w <- data.frame(wt = c(0.1, 0.5, 0.4, rep(0, (nrow(session1$iris)-3))))
+  dssUpload('w', datasources = opals[1])
+  cov_iris1 <- cov.wt(session1$iris[,1:4], wt = session1$w$wt )
+  rm(w)
+  rem <- dssCov('iris', type = 'split', wt = 'w$wt',  datasources = opals[1])
+  expect_true(all(rem$server1$cov - cov_iris1$cov == 0))
+})
