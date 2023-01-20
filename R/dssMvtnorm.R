635a041c99f5c6639f1357a649f112b833eea5a2
IulianD <iulian_dragan@yahoo.com>
2023-01-20 16:18:02 +0100

    dssMvtnorm, +sub,grep in dssDeriveColumn

---
+++ R/dssMvtnorm.R
@@ -0,0 +1,23 @@
+#' @title Call selected functions from the package mvtnorm
+#' @description At the moment only the function dmvnorm is implemented
+#' @param func a character, the name of the function to call
+#' @param ... further arguments to be passed to the function (see the documentation of the mvtnorm package).
+#' The object names present in the remote sessions must be sent as characters ('x' instead of x).
+#' @param async a logical, see datashield.aggregate
+
+#' @param  datasources a list of opal objects obtained after logging into the opal servers (see datashield.login)
+#'
+#' @export
+#'
+
+dssMvtnorm <- function(func, ..., async = TRUE, datasources = NULL){
+
+  if(is.null(datasources)){
+    datasources <- datashield.connections_find()
+  }
+  arglist <- .encode.arg(list(...)) # pass the args list almost as is to the original function on the local nodes
+  func <- .encode.arg(func)
+  cally <- paste0('mvtnormDSS("', func, '","', arglist, '")')
+  datashield.aggregate(datasources, as.symbol(cally), async = async)
+
+}
