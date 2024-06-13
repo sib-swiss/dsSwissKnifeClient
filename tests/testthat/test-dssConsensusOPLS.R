09b65ab605a49fdd945d143621b6e235379097e1
Iulian Dragan <iulian_dragan@yahoo.com>
2024-06-13 08:34:11 +0200

    test consensusOPLS

--- tests/testthat/test-dssConsensusOPLS.R
+++ tests/testthat/test-dssConsensusOPLS.R
@@ -2,5 +2,5 @@ test_that("dssConsensusOPLS works", {
   datashield.assign(opals['server1'], 'X', as.symbol('demo_3_Omics[1:3]'))
 
   x <- dssConsensusOPLS( async = FALSE, datasources = opals["server1"], data = 'X', Y='demo_3_Omics$Y')
-  expect_equal(names(x$server1), c("optimal","permuted", "permStats","plots"))
+  expect_equal(x$server1@modelType, 'da')
 })
